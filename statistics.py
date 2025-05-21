eb8952dd5c4032791a0ea952d57f2f8020f39f4e
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-21 21:10:38 +0200

    Added visual separator in printed results

--- statistics.py
+++ statistics.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
+import sqlite3
+from gestion import initialize_history, delete_history, log_listen
 from analyze import top_titles, top_artists, top_genres, total_unique_titles, total_unique_artists, total_unique_genres
 
 
 def main():
+	initialize_history()
+
+	print("--------------------------------------------------------------------------------------")
 	titles = top_titles()
-	print("---")
+	print("--------------------------------------------------------------------------------------")
 	artists = top_artists()
-	print("---")
+	print("--------------------------------------------------------------------------------------")
 	genres = top_genres()
-	print("---")
+	print("--------------------------------------------------------------------------------------")
 
 	title_count = total_unique_titles()
 	artist_count = total_unique_artists()
 	genre_count = total_unique_genres()
-	print(f"{title_count} titles from {artist_count} artists of {genre_count} genres")
+	print(f"{title_count} titles from {artist_count} artists of {genre_count} genres", end="\n\n")
 
 
 if __name__ == "__main__":
