b81e44c06e5f5f980649aabf9f8448f3515abefa
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-24 22:16:37 +0200

    Updated html/css/javascript

--- web/script.js
+++ web/script.js
@@ -1,7 +1,9 @@
 var year = new Date().getFullYear()
 var json = require(`../data/exports/${year}.json`);
+
 var count = json["count"]
 var titles = json["titles"]
 var artists = json["artists"]
 var genres = json["genres"]
+
 console.log(count, titles, artists, genres);
