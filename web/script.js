fcc4759557f0eea88fb9c7ccb004c0ddae5ecc91
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-24 17:41:25 +0200

    Added variables getting statistics from json file

--- web/script.js
+++ web/script.js
@@ -1,2 +1,7 @@
-var json = require("../data/exports/2025.json");
-console.log(json);
+var year = new Date().getFullYear()
+var json = require(`../data/exports/${year}.json`);
+var count = json["count"]
+var titles = json["titles"]
+var artists = json["artists"]
+var genres = json["genres"]
+console.log(count, titles, artists, genres);
