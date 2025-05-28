39d1997d58b8f4bd4840ac0ff577ab5bdbc7ed94
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-28 09:21:34 +0200

    Updated structure, style and behaviour

--- web/script.js
+++ web/script.js
@@ -1,9 +1,32 @@
-var year = new Date().getFullYear()
-var json = require(`../data/exports/${year}.json`);
+		let year = new Date().getFullYear();
+		let json = require(`../data/exports/${year}.json`);
 
+		document.getElementById('year').innerHTML = year;
+
+		function prevYear() {
+			year--;
+			document.getElementById('year').innerHTML = year;
+		}
+
+		function nextYear() {
+			year++;
+			document.getElementById('year').innerHTML = year;
+		}
+
+
+/*
 var count = json["count"]
 var titles = json["titles"]
 var artists = json["artists"]
 var genres = json["genres"]
 
+function prevCard() {
+	year--;
+}
+
+function nextCard() {
+	year++;
+}
+
 console.log(count, titles, artists, genres);
+*/
