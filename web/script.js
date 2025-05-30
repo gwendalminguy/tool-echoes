b8c11c1442952e11d621a259880bdb104b8a56cd
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-30 16:49:29 +0200

    Added JSON file reading and displaying

--- web/script.js
+++ web/script.js
@@ -1,32 +1,52 @@
-		let year = new Date().getFullYear();
-		let json = require(`../data/exports/${year}.json`);
+let year = new Date().getFullYear();
 
-		document.getElementById('year').innerHTML = year;
+function load() {
+	document.getElementById('year').innerHTML = year;
+	fetch(`../data/exports/${year}.json`)
+		.then(response => response.json())
+		.then(data => {
+			let i = 1;
+			for (const [key, value] of Object.entries(data.titles)) {
+				document.getElementById(`title-${i}`).textContent = `${value.artist} : ${value.title}`;
+				i++;
+			}
+			i = 1;
+			for (const [key, value] of Object.entries(data.artists)) {
+				document.getElementById(`artist-${i}`).textContent = `${value.artist} (${value.length} minutes)`;
+				i++;
+			}
+  		});
+}
 
-		function prevYear() {
-			year--;
-			document.getElementById('year').innerHTML = year;
-		}
+function prevYear() {
+	year--;
+	load();
+	document.getElementById('year').innerHTML = year;
+}
 
-		function nextYear() {
-			year++;
-			document.getElementById('year').innerHTML = year;
-		}
+function nextYear() {
+	year++;
+	load()
+	document.getElementById('year').innerHTML = year;
+}
 
+function prevCard() {
+	let json = require(`../data/exports/${year}.json`);
+	card = json["count"];
+	document.getElementById('card').innerHTML = card;
+}
+
+function nextCard() {
+	let json = require(`../data/exports/${year}.json`);
+	card = json.count.top_titles;
+	document.getElementById('card').innerHTML = card;
+}
 
 /*
 var count = json["count"]
 var titles = json["titles"]
 var artists = json["artists"]
 var genres = json["genres"]
-
-function prevCard() {
-	year--;
-}
-
-function nextCard() {
-	year++;
-}
 
 console.log(count, titles, artists, genres);
 */
