47768382ed3d1b8026a91c6b6c7ff98dc344f86c
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-01 12:01:17 +0200

    Added statistics.py automation

--- automation.sh
+++ automation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mkdir -p data/exports
-current=$(realpath ./src/log.py)
+log=$(realpath ./src/log.py)
+statistics=$(realpath ./src/statistics.py)
 python=$(which python3)
-(crontab -l 2>/dev/null ; echo "* * * * * $python $current") | crontab -
+(crontab -l 2>/dev/null; \
+echo "* * * * * $python $log"; \
+echo "* * * * 0 $python $statistics") | crontab -
