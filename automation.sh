fffa3cbad9d4f0ecb17ad32730e4bd003f75ca8d
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-07 18:18:31 +0200

    Renamed script statistics.py to extract.py

--- automation.sh
+++ automation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p data/exports
 log=$(realpath ./src/log.py)
-statistics=$(realpath ./src/statistics.py)
+extract=$(realpath ./src/extract.py)
 python=$(which python3)
 (crontab -l 2>/dev/null; \
 echo "* * * * * $python $log"; \
-echo "* * * * 0 $python $statistics") | crontab -
+echo "* * * * 0 $python $extract") | crontab -
