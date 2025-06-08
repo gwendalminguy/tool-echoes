02c2fa28db68054cd0d6b8a0a6ddf444d915991b
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-08 10:29:08 +0200

    Added symbolic link creation

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 mkdir -p data/exports
-log=$(realpath ./src/log.py)
-extract=$(realpath ./src/extract.py)
-python=$(which python3)
+ln -s ../data/exports web/exports
+LOG=$(realpath ./src/log.py)
+EXTRACT=$(realpath ./src/extract.py)
+PYTHON=$(which python3)
 (crontab -l 2>/dev/null; \
-echo "* * * * * $python $log"; \
-echo "* * * * 0 $python $extract") | crontab -
+echo "* * * * * $PYTHON $LOG"; \
+echo "* * * * 0 $PYTHON $EXTRACT") | crontab -
