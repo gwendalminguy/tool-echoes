4b538c194080ea8cb4d8cae848c091caa4cde822
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-09 21:37:05 +0200

    Updated crontab interval setting

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ EXTRACT=$(realpath ./src/extract.py)
 PYTHON=$(which python3)
 (crontab -l 2>/dev/null; \
 echo "* * * * * $PYTHON $LOG"; \
-echo "* * * * 0 $PYTHON $EXTRACT") | crontab -
+echo "0 * * * * $PYTHON $EXTRACT") | crontab -
