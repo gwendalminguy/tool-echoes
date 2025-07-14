12b457361aaac5b866e6fe61d0689967c11a963e
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-14 13:29:39 +0200

    Added blank line for clarity

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 mkdir -p data/exports
 ln -s ../data/exports web/exports
 chmod u+x run.sh
+
 LOG=$(realpath ./src/log.py)
 EXTRACT=$(realpath ./src/extract.py)
 PYTHON=$(which python3)
+
 (crontab -l 2>/dev/null; \
 echo "* * * * * $PYTHON $LOG"; \
 echo "0 * * * * $PYTHON $EXTRACT") | crontab -
