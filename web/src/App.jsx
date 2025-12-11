89f8353e45f12f5abc023524fe1e46f8369ba32b
gwendalminguy <gwendalminguy@outlook.fr>
2025-12-11 13:08:10 +0100

    Add App.jsx

---
+++ web/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import { StatisticsProvider } from "./context/StatisticsContext";
+
+import DashboardDisplay from "./components/DashboardDisplay";
+import YearSelector from "./components/YearSelector";
+
+import "./App.css";
+
+function App() {
+  const currentYear = new Date().getFullYear();
+  const [year, setYear] = useState(currentYear);
+
+  return (
+    <StatisticsProvider year={year}>
+      <div className="app">
+        <YearSelector year={year} setYear={setYear} />
+        <Dashboard />
+      </div>
+    </StatisticsProvider>
+  );
+}
+
+export default App;
