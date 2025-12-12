784d43e70f16f2ea4f0ebdd6ec11f220f9f061d6
gwendalminguy <gwendalminguy@outlook.fr>
2025-12-12 11:59:20 +0100

    Create NumberCard component

---
+++ web/src/components/cards/NumberCard.jsx
@@ -0,0 +1,14 @@
+function NumberCard({ cardClass, name, data, unit }) {
+
+  if (!data) return <div className={cardClass}>No Data</div>;
+
+  return (
+    <div className={cardClass}>
+      <h2 className="text-xl font-semibold mb-4">{name}</h2>
+		  <span className="text-4xl font-semibold">{data}</span>
+		  <span className="text-xl font-semibold"> {unit}</span>
+    </div>
+  );
+}
+
+export default NumberCard;
