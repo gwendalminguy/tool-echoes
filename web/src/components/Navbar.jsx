857122a04e6d385f58f3ecec4b0065e5cd5f6081
gwendalminguy <gwendalminguy@outlook.fr>
2025-12-12 21:44:38 +0100

    Remove ThemeContext

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,11 +1,7 @@
-import { useTheme } from "../context/ThemeContext";
-
 import YearSelector from "./YearSelector";
 import ThemeSwitch from "./ThemeSwitch";
 
 function Navbar({ year, setYear }) {
-  const { theme, toggleTheme } = useTheme();
-
   return (
     <nav className="navbar fixed top-0 left-0 w-full backdrop-blur-md bg-base-100/70 border-b border-base-300/70 shadow-sm z-100 h-20 transition-all duration-300">
       <div className="max-w-screen-7xl mx-auto px-4 h-full flex justify-between items-center w-full">
