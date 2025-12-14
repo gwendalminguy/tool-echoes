1d5e75cde9ea19253250d25ae5cfd490beb5786f
gwendalminguy <gwendalminguy@outlook.fr>
2025-12-14 18:30:29 +0100

    Correct TopCard component nam

--- web/src/components/DashboardDisplay.jsx
+++ web/src/components/DashboardDisplay.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 import { useStatistics } from "../context/StatisticsContext";
 
-import TopFiveCard from "./cards/TopFiveCard";
+import TopCard from "./cards/TopCard";
 import NumberCard from "./cards/NumberCard";
 import ItemCard from "./cards/ItemCard";
 import ChartCard from "./cards/ChartCard";
@@ -74,19 +74,18 @@ function DashboardDisplay() {
       <ItemCard cardClass={invisibleCard} data={totalUniqueGenres} unit="genres" />
       <ItemCard cardClass={invisibleCard} data={totalUniqueTitles} unit="titles" />
 
-      <TopFiveCard cardClass={visibleCard} name="Top Artists" data={dataArtists} unit="minutes" />
-      <TopFiveCard cardClass={visibleCard} name="Top Genres" data={dataGenres} unit="minutes" />
-      <TopFiveCard cardClass={visibleCard} name="Top Titles" data={dataTitles} unit="times" />
+      <TopCard cardClass={visibleCard} name="Top Artists" data={dataArtists} unit="minutes" />
+      <TopCard cardClass={visibleCard} name="Top Genres" data={dataGenres} unit="minutes" />
+      <TopCard cardClass={visibleCard} name="Top Titles" data={dataTitles} unit="times" />
 
       <NumberCard cardClass={visibleCard} name="Average Daily Count" data={averageDailyCount} unit="titles" />
       <NumberCard cardClass={visibleCard} name="Average Monthly Count" data={averageMonthlyCount} unit="titles" />
       <NumberCard cardClass={visibleCard} name="Total Count" data={totalCount} unit="titles" />
-
       <NumberCard cardClass={visibleCard} name="Average Daily Duration" data={averageDailyDuration} unit="minutes" />
       <NumberCard cardClass={visibleCard} name="Average Monthly Duration" data={averageMonthlyDuration} unit="minutes" />
       <NumberCard cardClass={visibleCard} name="Total Duration" data={totalDuration} unit="minutes" />
 
-      <ChartCard cardClass={visibleCard} />
+      <ChartCard cardClass={visibleCard} name="Chart" />
     </div>
   );
 }
