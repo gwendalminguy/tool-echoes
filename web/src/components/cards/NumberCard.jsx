e34c6c7bbc8134f9458b54b118a4b05176e1c56c
gwendalminguy <gwendalminguy@outlook.fr>
2025-12-15 16:43:02 +0100

    Improve design

--- web/src/components/cards/NumberCard.jsx
+++ web/src/components/cards/NumberCard.jsx
@@ -1,18 +1,30 @@
 function NumberCard({ cardClass, name, dataCount, dataDuration }) {
 
-  if (!dataCount || !dataDuration) return <div className={cardClass}>No Data</div>;
+  if (dataCount == null || dataDuration == null) return <div className={cardClass}>No Data</div>;
 
   return (
-    <div className={cardClass}>
+    <div className={`${cardClass} flex flex-col justify-between`}>
       <h2 className="text-lg font-semibold mb-4">{name}</h2>
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-row items-end gap-3">
-          <span className="text-4xl font-semibold">{dataCount.toLocaleString("fr-FR")}</span>
-          <span className="text-xl font-semibold"> titles</span>
+
+      <div className="grid grid-cols-2 gap-5">
+        {/* Count */}
+        <div className="flex flex-col">
+          <span className="text-sm uppercase font-semibold opacity-50 tracking-wide">
+            Titles
+          </span>
+          <span className="text-4xl font-bold leading-tight">
+            {dataCount.toLocaleString("fr-FR")}
+          </span>
         </div>
-        <div className="flex flex-row items-end gap-3">
-          <span className="text-4xl font-semibold">{dataDuration.toLocaleString("fr-FR")}</span>
-          <span className="text-xl font-semibold"> minutes</span>
+
+        {/* Duration */}
+        <div className="flex flex-col">
+          <span className="text-sm uppercase font-semibold opacity-50 tracking-wide">
+            Minutes
+          </span>
+          <span className="text-4xl font-bold leading-tight">
+            {dataDuration.toLocaleString("fr-FR")}
+          </span>
         </div>
       </div>
     </div>
