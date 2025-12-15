bb6bc49ced9d3594676f083adbb535f4cf30f494
gwendalminguy <gwendalminguy@outlook.fr>
2025-12-15 13:44:36 +0100

    Update icons

--- web/src/components/cards/ItemCard.jsx
+++ web/src/components/cards/ItemCard.jsx
@@ -1,10 +1,10 @@
-import { User, Guitar, Music } from "lucide-react";
+import { User, Music, AudioLines } from "lucide-react";
 
 function ItemCard({ cardClass, data, unit }) {
   const iconMap = {
     artists: User,
-    genres: Guitar,
-    titles: Music,
+    genres: Music,
+    titles: AudioLines,
   };
 
   const IconComponent = iconMap[unit];
@@ -12,8 +12,8 @@ function ItemCard({ cardClass, data, unit }) {
   if (!data) return <div className={cardClass}>No Data</div>;
 
   return (
-    <div className={`${cardClass} flex flex-col items-center text-center gap-3`}>
-      <div className="p-3 rounded-full bg-primary/20 text-primary">
+    <div className={`${cardClass} flex flex-col items-center text-center gap-1`}>
+      <div className="p-5 rounded-full bg-primary/20 text-primary mb-5">
         <IconComponent size={48} color="#4f46e5" />
       </div>
 		  <span className="text-4xl font-semibold">
