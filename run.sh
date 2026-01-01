994e1d2ca7a1e3d9b276849f3dbbfcdaca202c20
gwendalminguy <gwendalminguy@outlook.fr>
2026-01-01 18:36:38 +0100

    Update run procedure

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-open http://localhost:8000
-python3 -m http.server -d web
+# Setting Paths
+ROOT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WEB_DIRECTORY="$ROOT_DIRECTORY/web"
+
+# Starting Server
+echo "🟡 Starting server..."
+(
+  cd "$WEB_DIRECTORY"
+  npm run dev
+)
+
+# Opening Navigator
+/bin/sh -ec 'open http://localhost:5173'
